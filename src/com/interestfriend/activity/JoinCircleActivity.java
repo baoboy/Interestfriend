c4cb727f3da0df7859eab2ca0038c26fc740e2c1
binbin <774663576@qq.com>
2014-11-20 16:22:07 +0800

    a

--- src/com/interestfriend/activity/JoinCircleActivity.java
+++ src/com/interestfriend/activity/JoinCircleActivity.java
@@ -1,7 +1,6 @@
 package com.interestfriend.activity;
 
 import android.app.Dialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
